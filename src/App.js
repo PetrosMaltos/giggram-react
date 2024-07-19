976bab5bb1a19c3f5aa4c2b090e69cebcf9b800a
Your Name <you@example.com>
2024-07-19 15:41:55 +0500

    Your commit message

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import { useEffect } from 'react';
 import './App.css';
 const tg = window.Telegram.WebApp;
+
 function App() {
+  useEffect(() => {
+    tg.ready();
+  }, []);
 
-  useEffect( () => {
-      tg.ready();
-  }, [])
+  const onRegister = () => {
+    // Действие при нажатии на "Register"
+    console.log('Register button clicked');
+  };
 
-  const onClose = () => {
-      tg.close()
-  }
+  const onLogin = () => {
+    // Действие при нажатии на "Login"
+    console.log('Login button clicked');
+  };
 
   return (
     <div className="App">
-     work
-     <button onClick={onClose}>Close</button>
+      <div className="welcome-screen">
+        <h1>Welcome to GigGram!</h1>
+        <p>The first freelance platform on Telegram. Earn 100% with zero fees!</p>
+        <div className="button-container">
+          <button onClick={onRegister} className="btn">Register</button>
+          <button onClick={onLogin} className="btn">Login</button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Header = () => {
+    renurn (
+        <div className="header">
+            <button>Close</button>
+            <span className="username"></span>
+        </div>
+    );
+};
+
+
+export default Header;
