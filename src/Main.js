c4c5dd0d916bea386467396dcebb5e21a24fc72f
Your Name <you@example.com>
2024-07-19 23:08:27 +0500

    Updated project

--- src/Main.js
+++ src/Main.js
@@ -3,17 +3,12 @@ import React from 'react';
 import './Main.css';
 
 function Main() {
-  const handleResetClick = () => {
-    localStorage.clear(); // Очистка всего localStorage
-    window.location.reload(); // Перезагрузка страницы, чтобы отобразить изменения
-  };
-
   return (
     <div className="main-content">
       <h1>Main Content</h1>
       <p>Welcome to the main area of the application.</p>
-      <button className="btn btn-reset" onClick={handleResetClick}>Reset</button>
       {/* Add more content or components here */}
+      
     </div>
   );
 }
