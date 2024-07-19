8e46751f145b0d338a08e07b2ed2bc5fd78ee2cc
Your Name <you@example.com>
2024-07-19 22:52:53 +0500

    Updated project

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import './Main.css'; // Убедитесь, что у вас есть файл стилей для этого компонента
+import { useNavigate } from 'react-router-dom';
+import './Main.css'; // Импортируйте CSS файл для стилизации, если нужно
 
 function Main() {
+  const navigate = useNavigate();
+
+  const handleResetClick = () => {
+    localStorage.clear(); // Удаляет все данные из localStorage
+    navigate('/'); // Перенаправляет обратно на главный экран
+  };
+
   return (
-    <div className="main-content">
+    <div className="main">
       <h1>Main Content</h1>
       <p>Welcome to the main area of the application.</p>
-      {/* Добавьте сюда основной контент вашего приложения */}
+      <button className="btn btn-reset" onClick={handleResetClick}>Reset All Data</button>
     </div>
   );
 }
