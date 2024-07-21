3270912d1cf976e8e25cb397baa627758f614330
Your Name <you@example.com>
2024-07-22 00:05:18 +0500

    Updated project

--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react'; // Не забудьте импортировать useEffect
 import Navbar from './components/Navbar'; // Импортируем компонент Navbar
 import './Main.css'; // Импортируем стили для главной страницы
 
 const Main = () => {
   useEffect(() => {
+    // Проверяем наличие Telegram Web App SDK
     if (window.Telegram && window.Telegram.WebApp) {
+      // Устанавливаем цвет заголовка
       window.Telegram.WebApp.ready();
       window.Telegram.WebApp.setHeaderColor('#000000'); // Черный цвет
     }
   }, []);
-  
+
   return (
     <div className="main-page">
       <h1>Main Page</h1>
@@ -17,7 +19,6 @@ const Main = () => {
       <Navbar /> {/* Добавляем navbar внизу страницы */}
     </div>
   );
-  
 };
 
 export default Main;
