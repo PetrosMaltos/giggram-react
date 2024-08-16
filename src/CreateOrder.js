6effeab43c21ab697bf8fd94ed0a4b3fcc91396d
Your Name <you@example.com>
2024-08-16 12:07:51 +0700

    Updated project

--- src/CreateOrder.js
+++ src/CreateOrder.js
@@ -12,20 +12,24 @@ const CreateOrder = () => {
   });
 
   useEffect(() => {
+    // Настройка кнопки "Назад"
     if (window.Telegram && window.Telegram.WebApp) {
       window.Telegram.WebApp.BackButton.show();
-  
+
       const handleBackButtonClick = () => window.history.back();
       window.Telegram.WebApp.BackButton.onClick(handleBackButtonClick);
-  
-      // Cleanup function
+
       return () => {
-        if (window.Telegram && window.Telegram.WebApp) {
-          window.Telegram.WebApp.BackButton.offClick(handleBackButtonClick);
-          window.Telegram.WebApp.BackButton.hide();
-        }
+        window.Telegram.WebApp.BackButton.offClick(handleBackButtonClick);
+        window.Telegram.WebApp.BackButton.hide();
       };
     }
+
+    return () => {
+      if (window.Telegram && window.Telegram.WebApp) {
+        window.Telegram.WebApp.BackButton.hide();
+      }
+    };
   }, []);
   
   
