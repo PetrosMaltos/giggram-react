5dd42af43740b6e431ee9c66f7be420b7ca6cb05
Your Name <you@example.com>
2024-08-17 19:46:55 +0700

    Updated project

--- src/MyProjects.js
+++ src/MyProjects.js
@@ -17,17 +17,8 @@ const MyProjects = () => {
       const handleBackButtonClick = () => window.history.back();
       window.Telegram.WebApp.BackButton.onClick(handleBackButtonClick);
 
-      return () => {
-        window.Telegram.WebApp.BackButton.offClick(handleBackButtonClick);
-        window.Telegram.WebApp.BackButton.hide();
-      };
     }
 
-    return () => {
-      if (window.Telegram && window.Telegram.WebApp) {
-        window.Telegram.WebApp.BackButton.hide();
-      }
-    };
   }, []);
 
   return (
